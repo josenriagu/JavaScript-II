ae8ae25a8e7b73c232a0a1d5a28f5c7abe256324
Josemaria <josemarianriagu25@gmail.com>
2019-05-07 15:53:13 +0100

    - Implement IIFE to challenge 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let firstname = 'josemaria';
+let lastname = 'nriagu';
 
+const output = function printName() {
+  nickname = 'Dr. Flynn';
+  return `The name of ${nickname}'s branch is '${firstname}-${lastname}'`
+}
+
+console.log(output());
+
+// Using IIFE method
+const output2 = (() => {
+  nickname = 'Dr. Flynn';
+  console.log (`The name of ${nickname}'s branch is '${firstname}-${lastname}'`)
+})();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,7 +22,21 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  function inner() {
+    count = count + 1;
+    console.log(count);
+  }
+  return inner;
 };
+
+const newCounter = counter();
+
+newCounter();
+newCounter();
+newCounter();
+newCounter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,4 +46,9 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let initial = 0;
+  return () => {
+    initial = initial + 1;
+    console.log(initial)
+  }
 };
